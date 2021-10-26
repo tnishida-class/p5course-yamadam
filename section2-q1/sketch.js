e70df541c26b31260d768d01d4f98415ef669860
yamadam <2070524h@stu.kobe-u.ac.jp>
2021-10-26 10:03:10 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,14 +1,15 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
     noFill();
-    ellipse(50, 50, i * 10 + 10);
 
-    if(i < 4){
-    stroke(0, 0, 255);
-　  }
-    else if(i < 10){
-    stroke(255, 0, 0);
+    for(let i = 0; i < 10; i++){
+      let r = i * 10 + 10;
+      if(i < 5){
+        stroke(0, 0, 255);
+　    }
+      else{
+        stroke(255, 0, 0);
+      }
+      ellipse(50, 50, r);
     }
   }
-}
