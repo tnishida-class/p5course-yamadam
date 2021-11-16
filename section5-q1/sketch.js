fa895ae4c11efc62972d6c1655121a71a86ffe1a
yamadam <2070524h@stu.kobe-u.ac.jp>
2021-11-16 11:50:36 +0900

    Change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -3,16 +3,22 @@
 // 吹き出しにしっぽを付けてみよう
 function setup(){
   createCanvas(400, 400);
-  background(255);
-  balloon("I love keyakizaka46");
 }
 
-function balloon(t){
+function draw(){
+  background(255);
+  balloon("I love keyakizaka46", mouseX, mouseY);
+}
+
+function balloon(t, x, y){
+  push();
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  rect(x, y, w + p * 2, h + p * 2);
+  triangle(x + 5, y + h, x + 5, y + h + 10, x + 15, y + h);
   fill(255);
-  text(t, p, h + p);
+  text(t, x + p, y + h + p);
+  pop();
 }
